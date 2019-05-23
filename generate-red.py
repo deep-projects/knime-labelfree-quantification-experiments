8491170c246c5b0197570d194a0308736633f4b3
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2019-05-23 12:47:11 +0200

    listing

--- generate-red.py
+++ generate-red.py
@@ -19,7 +19,49 @@ red = {
                     'url': 'https://s3.denbi.uni-tuebingen.de/fb-test/demo_container/cibi_user_meeting_2018/labelfree_example.tar.gz',
                     'archiveFormat': 'gztar'
                 }
-            }
+            },
+            'listing': [{
+                'class': 'Directory',
+                'basename': 'datasets',
+                'listing': [{
+                    'class': 'File',
+                    'basename': 'lfq_spikein_dilution_1.mzML'
+                }, {
+                    'class': 'File',
+                    'basename': 'lfq_spikein_dilution_2.mzML'
+                }, {
+                    'class': 'File',
+                    'basename': 'lfq_spikein_dilution_3.mzML'
+                }]
+            }, {
+                'class': 'Directory',
+                'basename': 'databases',
+                'listing': [{
+                    'class': 'File',
+                    'basename': 's_pyo_sf370_potato_human_target_decoy_with_contaminants.fasta'
+                }, {
+                    'class': 'File',
+                    'basename': 's_pyo_sf370_potato_human_target_decoy_with_contaminants.fasta.phr'
+                }, {
+                    'class': 'File',
+                    'basename': 's_pyo_sf370_potato_human_target_decoy_with_contaminants.fasta.pin'
+                }, {
+                    'class': 'File',
+                    'basename': 's_pyo_sf370_potato_human_target_decoy_with_contaminants.fasta.psq'
+                }, {
+                    'class': 'File',
+                    'basename': 'uniprot-s-pyogenes-sf370-plus-potato-and-human-spike-ins.fasta'
+                }, {
+                    'class': 'File',
+                    'basename': 'uniprot-s-pyogenes-sf370-plus-potato-and-human-spike-ins.fasta.phr'
+                }, {
+                    'class': 'File',
+                    'basename': 'uniprot-s-pyogenes-sf370-plus-potato-and-human-spike-ins.fasta.pin'
+                }, {
+                    'class': 'File',
+                    'basename': 'uniprot-s-pyogenes-sf370-plus-potato-and-human-spike-ins.fasta.psq'
+                }]
+            }]
         },
         'database_name': 's_pyo_sf370_potato_human_target_decoy_with_contaminants.fasta'
     },
